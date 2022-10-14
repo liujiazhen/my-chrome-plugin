6bf04a2d786715a9688045ad47ab1c7e310e4095
liujiazhen <liujiazhen@live.cn>
2022-10-14 18:42:12 +0800

    baidu

--- js/content.js
+++ js/content.js
@@ -1,12 +1,18 @@
 (function () {
 	let page = window.location.href
+
+    let myChinaText = "富强、民主、文明、和谐  自由、平等、公正、法治  爱国、敬业、诚信、友善"
+
     var ZhiHu = {
         init: function () {
-        	console.log(document.title);
+        	console.log(document.title + ' ———— ' + '刘加振');
 
+            // 头标签隐藏
             $('header').remove();
+
         	if (page.includes('question') || page.includes('answer')) {
                 // 详情页面
+
                 // 侧边栏
                 document.querySelector('div.Question-sideColumn').style.display="none";
 
@@ -14,6 +20,9 @@
                     ZhiHu.buttonHidden()
                     ZhiHu.authorInfoHidden()
                 }, 2000)
+
+                // 回答列表，居中
+                document.querySelector('div.Question-main').style.margin="10px 10px 10px 400px";
         	} else {
                 // 首页
 
@@ -28,35 +37,51 @@
                 let childrenChildren = container.children();
                 childrenChildren[1].remove();
 
-                setInterval(function() {
-                    ZhiHu.buttonHidden()
+                if (page.includes('hot')) {
                     ZhiHu.itemHotHidden()
-                }, 2000)
+                } else {
+                    setInterval(function() {
+                        ZhiHu.buttonHidden()
+                    }, 2000)
+                }
         	}
         },
         // 按钮栏隐藏
         buttonHidden: function () {
             let buttonCardList = $('.ContentItem-actions');
-            for (var i = buttonCardList.length - 1; i >= 0; i--) {
+
+            for (var i = 0; i < buttonCardList.length; i++) {
                 let buttonList = buttonCardList[i];
                 let buttonListChildren = buttonList.children;
-                buttonListChildren[0].style.display = "none" // 点赞
-                // buttonListChildren[1].style.display = "none" // 评论
-                buttonListChildren[2].style.display = "none" // 分享
-                buttonListChildren[3].style.display = "none" // 收藏
-                buttonListChildren[4].style.display = "none" // 喜欢
-                buttonListChildren[5].style.display = "none" // 更多
+
+                // buttonListChildren[0].style.display = "none" // 点赞
+                // // buttonListChildren[1].style.display = "none" // 评论
+                // buttonListChildren[2].style.display = "none" // 分享
+                // buttonListChildren[3].style.display = "none" // 收藏
+                // buttonListChildren[4].style.display = "none" // 喜欢
+                // buttonListChildren[5].style.display = "none" // 更多
+
+                if (buttonListChildren.length > 2) {
+                    console.log('按钮列表' + i)
+                    buttonListChildren[5].remove() // 更多
+                    buttonListChildren[4].remove() // 喜欢
+                    buttonListChildren[3].remove() // 收藏
+                    buttonListChildren[2].remove() // 分享
+                    buttonListChildren[0].remove() // 点赞
+                    // buttonListChildren[1].remove() // 评论
+                }
             }
         },
         // 答主信息隐藏
         authorInfoHidden: function () {
             // 关注按钮
             let followBtnList = document.querySelectorAll('.FollowButton');
-            console.log(followBtnList.length)
             for (var i = 0; i < followBtnList.length; i++) {
-                followBtnList[i].style.color="red";
-                followBtnList[i].disable="true";
-                followBtnList[i].innerHTML="富强、民主、文明、和谐  自由、平等、公正、法治  爱国、敬业、诚信、友善";
+                if (followBtnList[i].innerHTML != myChinaText) {
+                    followBtnList[i].style.color = "red";
+                    followBtnList[i].disable = "true";
+                    followBtnList[i].innerHTML = myChinaText;
+                }
             }
             // 作者信息
             let authorInfoList = document.querySelectorAll('.AuthorInfo');
@@ -78,13 +103,14 @@
                         if (hotItemChildren[1] != null && hotItemChildren[1] != undefined) {
                             // 热榜内容
                             let hotItemContent = hotItemChildren[1];
-                            hotItemContent.style.color = 'green'
+                            // hotItemContent.style.color = 'green'
                             hotItemContent.style.background = '#eef0f4'
+                            // hotItemContent.style.background = '#66ccff'
 
                             let hotItemContentChildren = hotItemContent.children;
                             if (hotItemContentChildren.length > 1) {
-                                hotItemContentChildren[0].style.color = 'green'
-                                hotItemContentChildren[1].remove();
+                                // hotItemContentChildren[0].style.color = 'green'
+                                hotItemContentChildren[1].style.display = "none";
                             }
                         }
 
@@ -94,9 +120,9 @@
                         if (hotItemChildren[0] != null) {
                             hotItemChildren[0].remove(); // 热榜编号
                         }
+
+                        hotItem.style.padding = "30px"
                     }
-                    
-                    hotItem.style.padding = "30px"
                 }
             }
         }
--- js/site.js
+++ js/site.js
@@ -1,3 +1,14 @@
 console.log(document.title);
-document.querySelector('aside').innerHTML="";
-document.querySelector('main').style.width="100%";
+
+let aside = document.querySelector('aside');
+if (aside != null && aside != undefined) {
+	aside.innerHTML = "";
+}
+
+let pageMain = document.querySelector('main');
+if (pageMain != null && pageMain != undefined) {
+	pageMain.style.width = "100%";
+}
+
+// document.querySelector('aside').innerHTML="";
+// document.querySelector('main').style.width="100%";
